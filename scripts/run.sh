5bb2360617bd8b04a107060ab58534c31ff0f897
Anton <antonikwiatek47@outlook.com>
2024-12-18 00:52:15 +0100

    Script to compare outputs

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+counter=1
+
+x=$(dirname $(pwd))
+
 while true
 do
-        # command to be executed
+        make -C $x all
+
+        if cmp -s "$x/tests/output_brute_force.txt" "$x/tests/output_solution.txt"; then
+                echo "Test nr '$counter': PASSED"
+        else
+                echo "Test nr '$counter': FAILED"
+                break
+        fi
         
+        ((counter++))
 done
