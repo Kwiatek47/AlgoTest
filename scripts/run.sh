d2c1dcffbbb76549de7fd2aee30bf7b2f4d82e27
Anton <antonikwiatek47@outlook.com>
2024-12-18 21:57:22 +0100

    New optimized concept main loop

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
+# Initizalize counter
 counter=1
 
+# Get the parent dir of the current one to get access to makefile 
 x=$(dirname $(pwd))
 
+# Compile generator brute and solution
+make -C $x compile_GSB
+
+# Main loop
 while true
 do
-        make -C $x all
+        # /dev/null 2>&1 to hide unnecessary paths and other shit
+        make -C $x run_GSB > /dev/null 2>&1
 
+        # Compare two output files
         if cmp -s "$x/tests/output_brute_force.txt" "$x/tests/output_solution.txt"; then
-                echo "Test nr '$counter': PASSED"
+                echo "Test nr '$counter': PASSED ✅"
         else
-                echo "Test nr '$counter': FAILED"
+                echo "Test nr '$counter': FAILED ❌"
                 break
         fi
-        
+
         ((counter++))
 done
+
+make -C $x clean > /dev/null 2>&1
